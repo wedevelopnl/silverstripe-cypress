33179bd1f29731932952c5bcfb0257410e71be59
Erik Frèrejean <erikfrerejean@users.noreply.github.com>
2024-06-18 16:48:39 +0200

    Add helper to add elements to a grid page

--- src/Traits/ElementalGridFixtureTrait.php
+++ src/Traits/ElementalGridFixtureTrait.php
@@ -4,16 +4,19 @@ declare(strict_types=1);
 
 namespace WeDevelop\Cypress\Traits;
 
+use App\Pages\GridPage;
 use DNADesign\Elemental\Extensions\ElementalPageExtension as DNAExtensionsElementalPageExtension;
+use DNADesign\Elemental\Models\BaseElement;
 use DNADesign\Elemental\Models\ElementalArea;
+use SilverStripe\CMS\Model\SiteTree;
 use WeDevelop\ElementalGrid\Extensions\ElementalPageExtension as WeDevelopExtensionsElementalPageExtension;
 
 trait ElementalGridFixtureTrait {
     /**
-     * @param class-string $class
+     * @param class-string $className
      */
-    protected function createPageWithGrid(string $className, ...$args) {
-        $page = call_user_func(sprintf('%s::create', $className), $args);
+    protected function createPageWithGrid(string $className, ...$args): SiteTree {
+        $page = call_user_func_array(sprintf('%s::create', $className), $args);
         $page->write();
 
         if (
@@ -28,5 +31,18 @@ trait ElementalGridFixtureTrait {
         $elementalArea->OwnerID = $page->ID;
         $page->ElementalAreaID = $elementalArea->write();
         $page->write();
+
+        return $page;
+    }
+
+    /**
+     * @param class-string $elementClassName
+     */
+    protected function addElementToPage(SiteTree $page, string $elementClassName, ...$args): void {
+        $elementArgs = ($args[0] ?? []) + ['ParentID' => $page->ElementalAreaID];
+
+        /** @var BaseElement $element */
+        $element = call_user_func(sprintf('%s::create', $elementClassName), $elementArgs);
+        $element->write();
     }
 }
